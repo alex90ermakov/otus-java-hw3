7fd362c7ed30e2c78637b95d27f5aa2b3e18e509
alex90ermakov <ealeksey1409@mail.ru>
2024-12-27 02:15:57 +0300

    добавил интерфейс Flying и переопределил метод say для класса Duck

--- src/main/java/animals/bird/Duck.java
+++ src/main/java/animals/bird/Duck.java
@@ -6,4 +6,17 @@ public class Duck extends Animal {
     public Duck(String name, int age, int weight, String color) {
         super(name, age, weight, color);
     }
+
+    //метод fly
+    public void fly() {
+        System.out.println("Я лечу");
+    }
+
+    //переопределение метода
+    @Override
+    public void say(){
+        System.out.println("Кря");
+    }
+
+
 }
---
+++ src/main/java/animals/bird/Flying.java
@@ -0,0 +1,5 @@
+package animals.bird;
+
+public interface Flying {
+    void fly();
+}
