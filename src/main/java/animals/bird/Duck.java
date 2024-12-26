501bc48ff0452ccfcf2acd60601d0892175529a3
alex90ermakov <ealeksey1409@mail.ru>
2024-12-27 01:49:05 +0300

    добавл класс Duck

---
+++ src/main/java/animals/bird/Duck.java
@@ -0,0 +1,9 @@
+package animals.bird;
+
+import animals.Animal;
+
+public class Duck extends Animal {
+    public Duck(String name, int age, int weight, String color) {
+        super(name, age, weight, color);
+    }
+}
