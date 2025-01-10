d7cad8f31452f8d1f2a8d30041bd55ba6aa46953
alex90ermakov <ealeksey1409@mail.ru>
2025-01-10 12:43:32 +0300

    Переопределил метод Say для Cat и Dog

--- src/main/java/animals/pets/Cat.java
+++ src/main/java/animals/pets/Cat.java
@@ -6,4 +6,9 @@ public class Cat extends Animal {
     public Cat(String name, int age, int weight, String color) {
         super(name, age, weight, color);
     }
+
+    @Override
+    public void say(){
+        System.out.println("Мяу");
+    }
 }
--- src/main/java/animals/pets/Dog.java
+++ src/main/java/animals/pets/Dog.java
@@ -6,4 +6,9 @@ public class Dog extends Animal {
     public Dog(String name, int age, int weight, String color) {
         super(name, age, weight, color);
     }
+
+    @Override
+    public void say(){
+        System.out.println("Гав");
+    }
 }
